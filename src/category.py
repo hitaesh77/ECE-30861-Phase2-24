d93957b1f720dcdffedcc3fc240a1db77cf5e34a
shxrkss <mchuang67@gmail.com>
2025-09-27 15:57:42 -0400

    Updated for new standards

--- src/category.py
+++ src/category.py
@@ -1,12 +1,22 @@
 # category.py
 
+import time
 from run import UrlCategory
 
-def compute(payload: dict) -> str:
+def compute(model_url: str, code_url: str, dataset_url: str) -> str:
     """
     Returns the category of model.
     """
-    category = payload.get("category")
-    if not category:
-        return UrlCategory.OTHER.value
-    return category
+    startTime = time.time()
+
+    if model_url and "huggingface.co" in model_url:
+        category = "MODEL"
+    elif code_url and "github.com" in code_url:
+        category = "CODE"
+    elif dataset_url and "huggingface.co/datasets" in dataset_url:
+        category = "DATASET"
+    else:
+        category = "OTHER"
+
+    latency_ms = (time.time() - startTime) * 1000
+    return category, latency_ms
