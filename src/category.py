3083967cea285488dc467c139ae5d41d9d418084
shxrkss <mchuang67@gmail.com>
2025-09-27 16:10:09 -0400

    Updated category to return MODEL

--- src/category.py
+++ src/category.py
@@ -3,20 +3,10 @@
 import time
 from run import UrlCategory
 
-def compute(model_url: str, code_url: str, dataset_url: str) -> str:
+def compute(model_url: str, code_url: str, dataset_url: str) -> dict:
     """
     Returns the category of model.
     """
     startTime = time.time()
-
-    if model_url and "huggingface.co" in model_url:
-        category = "MODEL"
-    elif code_url and "github.com" in code_url:
-        category = "CODE"
-    elif dataset_url and "huggingface.co/datasets" in dataset_url:
-        category = "DATASET"
-    else:
-        category = "OTHER"
-
     latency_ms = (time.time() - startTime) * 1000
-    return category, latency_ms
+    return "MODEL", latency_ms
